82126b0dd408d1f536bb008511423e99faeabe3c
nhat-nguyen-unified <nhat.nguyen@unified.com>
2023-02-25 22:05:02 +0700

    commit

--- code.js
+++ code.js
@@ -38,13 +38,166 @@ const constants = {
   VCOMIC_PASSWORD: "vpassword",
 };
 
-// import md5 from "md5";
+const getChapterConfig = (cookie, comicId) => ({
+  method: "get",
+  url: `https://vcomic.net/app/manga/controllers/cont.getChapterList.php?comicId=${comicId}`,
+  headers: {
+    cookie,
+  },
+});
 
-// import axios from "axios";
+const readChapterConfig = (cookie, url) => ({
+  method: "head",
+  url: `https://vcomic.net${url}`,
+  headers: {
+    cookie,
+  },
+});
 
-// import * as md5 from "./node_modules/md5/md5.js";
+const getPointConfig = (cookie, data) => ({
+  method: "post",
+  url: "https://vcomic.net/app/manga/controllers/cont.updateLevel.php?type=chapter",
+  headers: {
+    cookie,
+  },
+  data,
+});
 
-import { VComic } from "./test.js";
+const getTokenConfig = (PHPSESSID) => ({
+  method: "get",
+  url: "https://vcomic.net/account/login",
+  headers: {
+    Cookie: `PHPSESSID=${PHPSESSID}`,
+  },
+});
+
+const getLoginConfig = (token, PHPSESSID, data) => ({
+  method: "post",
+  url: `https://vcomic.net/controllers/cont.userForm.php?action=login&token=${token}`,
+  headers: {
+    Cookie: `PHPSESSID=${PHPSESSID}`,
+    // ...data.getHeaders(),
+  },
+  data,
+});
+
+const PHPSESSIDGenerator = (length) => {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  var result = "";
+  for (var i = 0; i < length; ++i) {
+    result += alphabet[Math.floor(alphabet.length * Math.random())];
+  }
+  return result;
+};
+
+const getToken = (htmlString) => {
+  const firstMatched =
+    /cont\.userForm\.php\?action=login&token=[a-z0-9]*/g.exec(htmlString)[0];
+
+  const secondMatched = /token=[a-z0-9]*/g.exec(firstMatched)[0];
+
+  return secondMatched.split("=")[1];
+};
+
+const getLoginForm = () => {
+  const data = new FormData();
+  data.append("email", "ltwnhattan+2@gmail.com");
+  data.append("password", "0502ngocty");
+  data.append("isRemember", "on");
+  return data;
+};
+
+let count = 0;
+
+const VComic = async (comicId) => {
+  try {
+    const PHPSESSID = PHPSESSIDGenerator(26);
+
+    const htmlString = await (
+      await axios.request(getTokenConfig(PHPSESSID))
+    ).data;
+
+    const token = getToken(htmlString);
+    const data = getLoginForm();
+
+    const activateSessionResponse = await (
+      await axios.request(getLoginConfig(token, PHPSESSID, data))
+    ).data;
+
+    if (activateSessionResponse === "...") {
+      console.log("PHPSESSID", PHPSESSID);
+      const cookie = `PHPSESSID=${PHPSESSID}`;
+
+      const chapters = await (
+        await axios.request(getChapterConfig(cookie, comicId))
+      ).data.chapters;
+      console.log(chapters);
+      // chapters.forEach(async ({ url, chapterId }) => {
+      //   try {
+      //     await axios.request(readChapterConfig(cookie, url));
+
+      //     const response = await axios.request(
+      //       // this one can get from updateChapter endpoint
+      //       getPointConfig(
+      //         cookie,
+      //         `chapterId=${chapterId}&levelToken=dc6f955c58`
+      //       )
+      //     );
+
+      //     if (response.data["success"]) {
+      //       console.log(++count);
+      //     }
+      //   } catch (error) {
+      //     // console.log(error);
+      //     return;
+      //   }
+      // });
+    } else {
+      console.log("failed to activate session");
+    }
+  } catch (error) {}
+};
+
+// const constants = {
+//   GET_CHAPTER: "GET_CHAPTER",
+//   READ_CHAPTER: "READ_CHAPTER",
+//   GET_POINT: "GET_POINT",
+//   GET_TOKEN: "GET_TOKEN",
+//   GET_LOGIN: "GET_LOGIN",
+// };
+
+// const config = {
+//   GET_CHAPTER: (comicId) => ({
+//     method: "get",
+//     url: `https://vcomic.net/app/manga/controllers/cont.getChapterList.php?comicId=${comicId}`,
+//   }),
+//   READ_CHAPTER: (url) => ({
+//     method: "head",
+//     url: `https://vcomic.net${url}`,
+//   }),
+//   GET_POINT: (data) => ({
+//     method: "post",
+//     url: "https://vcomic.net/app/manga/controllers/cont.updateLevel.php?type=chapter",
+//     data,
+//   }),
+//   GET_TOKEN: () => ({
+//     method: "get",
+//     url: "https://vcomic.net/account/login",
+//   }),
+//   GET_LOGIN: (token, data) => ({
+//     method: "post",
+//     url: `https://vcomic.net/controllers/cont.userForm.php?action=login&token=${token}`,
+//     data,
+//   }),
+// };
+
+// const getConfig = (name, PHPSESSID) => ({
+//   ...config(name),
+//   headers: {
+//     Cookie: `PHPSESSID=${PHPSESSID}`,
+//   },
+// });
 
 const onSubmit = async () => {
   // const email = document.getElementById(constants.EMAIL).value;
